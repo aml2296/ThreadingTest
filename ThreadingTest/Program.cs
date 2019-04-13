fe04706b78ad4d9c6668d2f4b22a590c0dafc85d
Aaron Loux <louxaaron@yahoo.com>
2019-04-13 12:48:45 -0500

    .

---
+++ ThreadingTest/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+namespace ThreadingTest
+{
+    class ThreadHandler
+    {
+        public delegate bool endCount();
+        public static endCount endOfCount = new endCount(EndCountMessage);
+
+
+        //Create a MainThread to output int
+        //Create a OtherThread to take in user input
+        public static void ThreadCounter()
+        { 
+            int countAmt = 10;
+            int sleepAmt = 1000;
+
+            Console.WriteLine("Begin Count!");
+            for (int i = 1; i <= countAmt; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(sleepAmt);
+            }
+            endOfCount.Invoke();
+        }
+        public static bool EndCountMessage()
+        {
+            try
+            {
+                Console.WriteLine("End of Count!");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            return true;
+        }
+
+
+
+        /*static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ThreadStart countThread = new ThreadStart(ThreadCounter);
+
+            do
+            {
+                Console.WriteLine("Do you want to count?");
+                Console.WriteLine("Any Key to continue...");
+                Console.ReadKey();
+                bool loop = true;
+                Thread counter = new Thread(countThread);
+                counter.Start();
+                do
+                {
+                    Console.ReadKey();
+                } while (loop != false);
+                counter.Abort();
+            } while (1 < 0);
+
+        }*/
+    }
+}
---
+++ ThreadingTest/pick-up-ball.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThreadingTest
+{
+    class pick_up_ball_DISPLAY
+    {
+        char[,] theGrid;
+        const int floorLevel = 4;
+        displayObject floor;
+        displayObject ball;
+        displayObject man;
+
+        pick_up_ball_DISPLAY(int x, int y) //size of grid x by y
+        {
+            theGrid = new char[x, y];
+            InitalizeObjects();
+        }
+
+        public void InitalizeObjects()
+        {
+            floor = new displayObject(0, theGrid.GetLength(0), theGrid.GetLength(1));
+            for (int i = floorLevel; i > 0; i--)
+            {
+                char valueOfIndex = 'X';
+                if (i == floorLevel)
+                    valueOfIndex = '=';
+                for (int x = 0; x < theGrid.GetLength(0); x++)
+                    floor.setValues(ref theGrid, new displayObject.index(valueOfIndex, x, i));
+            }
+            int j = 0;
+        }
+        class displayObject //Base class for Objects to Draw
+        {
+            public int layer;
+            int[,] picValues;
+
+            public displayObject(int l, int x, int y)
+            {
+                layer = l;
+                picValues = new int[x, y];
+            }
+            public void setValues(ref char[,] TheGrid, params index[] indexList)
+            {
+                foreach (index ind in indexList)
+                {
+                    if (ind.x >= 0 && ind.x <= TheGrid.GetLength(0))
+                        TheGrid[ind.x, ind.y] = ind.value;
+                    else if (ind.y >= 0 && ind.y <= TheGrid.GetLength(1))
+                        TheGrid[ind.x, ind.y] = ind.value;
+                }
+            }
+            public class index
+            {
+                public char value;
+                public int  x, y;
+                public index(char _value, int _x,int _y)
+                {
+                    value = _value;
+                    x = _x;
+                    y = _y;
+                }
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            pick_up_ball_DISPLAY display = new pick_up_ball_DISPLAY(15,15);
+        }
+    }
+}
